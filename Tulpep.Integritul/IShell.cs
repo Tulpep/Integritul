9518ce0ccbad07160f747ccaeb3437d54eb653d2
Ricardo Polo <ricardo.polo@tulpep.com>
2015-02-15 17:19:58 -0500

    Caliburn Micro

---
+++ Tulpep.Integritul/IShell.cs
@@ -0,0 +1,3 @@
+namespace Tulpep.Integritul {
+    public interface IShell {}
+}
---
+++ Tulpep.Integritul/ShellViewModel.cs
@@ -0,0 +1,3 @@
+namespace Tulpep.Integritul {
+    public class ShellViewModel : Caliburn.Micro.PropertyChangedBase, IShell { }
+}
